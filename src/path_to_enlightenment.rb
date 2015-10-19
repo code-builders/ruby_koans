30c1b798acd46cd838aaf3a308e3fad1b998a78e
Bookis Smuin <bookis.smuin@gmail.com>
2015-10-18 20:45:24 -0700

    move about nil down the line

--- src/path_to_enlightenment.rb
+++ src/path_to_enlightenment.rb
@@ -3,7 +3,6 @@
 $LOAD_PATH << File.dirname(__FILE__)
 
 require 'about_asserts'
-require 'about_nil'
 require 'about_objects'
 require 'about_arrays'
 require 'about_array_assignment'
@@ -20,6 +19,7 @@ require 'about_control_statements'
 require 'about_true_and_false'
 require 'about_triangle_project'
 require 'about_exceptions'
+require 'about_nil'
 require 'about_triangle_project_2'
 require 'about_iteration'
 require 'about_blocks'
